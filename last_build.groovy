21c17a525bd3823cf0ada5a0c232671a0381a33b
Aizada Kanybekkyzy <aizadushka87@gmail.com>
2020-04-02 19:51:07 -0500

    update
    Signed-off-by: Aizada Kanybekkyzy <aizadushka87@gmail.com>

---
+++ last_build.groovy
@@ -0,0 +1,50 @@
+node { 
+   
+    properties([
+        // Keeps only  last 5 builds
+        buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
+        pipelineTriggers([
+            // Runs with "Input" JOb
+            upstream('Input'),
+            // Execute every minute
+             cron('* * * * *')])
+
+            ]),
+        ])     
+        
+        
+stage("Stage1"){ 
+
+git 'https://github.com/farrukh90/packer.git' 
+
+} 
+
+stage("Stage2"){ 
+
+echo "hello" 
+
+} 
+
+stage("Stage3"){ 
+
+echo "hello" 
+
+} 
+
+stage("Stage4"){ 
+
+echo "hello" 
+
+} 
+
+stage("Stage5"){ 
+
+node { 
+
+// some block 
+
+}  
+
+} 
+
+} 
