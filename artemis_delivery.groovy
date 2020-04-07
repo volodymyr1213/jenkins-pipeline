bb5263517589d35f2bee33604389fa059548f680
Aizada Kanybekkyzy <aizadushka87@gmail.com>
2020-04-07 18:55:09 -0500

    update
    Signed-off-by: Aizada Kanybekkyzy <aizadushka87@gmail.com>

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -13,34 +13,25 @@ node {
 	stage("Stage1"){
 		timestamps {
 			ws {
-                checkout([$class: 'GitSCM', branches: [[name: '${Version}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/fuchicorp/artemis.git']]]) 
-
-		}
+                checkout([$class: 'GitSCM', branches: [[name: '${Version}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/fuchicorp/artemis.git']]])		}
 	}
 }
 	stage("Get Credentials"){
 		timestamps {
 			ws{
 				sh '''
-					aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 965334959964.dkr.ecr.us-east-1.amazonaws.com/artemis
+                aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 965334959964.dkr.ecr.us-east-1.amazonaws.com/artemis
 					'''
 		    }
 	    }
     }
-    stage("Build Docker Image"){ 
-
-timestamps { 
-
-ws { 
-
-sh ''' 
-
-docker build -t artemis:${Version} . 
-
-''' 
-
-} 
-
-} 
-}
+    stage("Build Docker Image"){
+		timestamps {
+			ws {
+				sh '''
+					docker build -t artemis:${Version} .
+					'''
+		    }
+	    }
+    }
 }
