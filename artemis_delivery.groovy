e784b03fd8cff19e7c963951a5d571e96cf93196
Aizada Kanybekkyzy <aizadushka87@gmail.com>
2020-04-07 19:41:18 -0500

    update
    Signed-off-by: Aizada Kanybekkyzy <aizadushka87@gmail.com>

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -13,14 +13,14 @@ node {
 	stage("Stage1"){
 		timestamps {
 			ws {
-                checkout([$class: 'GitSCM', branches: [[name: 'dev']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/artemis.git']]])		}
+                checkout([$class: 'GitSCM', branches: [[name: '${Version}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/artemis.git']]])		}
 	}
 }
 	stage("Get Credentials"){
 		timestamps {
 			ws{
 				sh '''
-					aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 965334959964.dkr.ecr.us-east-1.amazonaws.com/artemis
+                aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 965334959964.dkr.ecr.us-east-1.amazonaws.com/artemis
 					'''
 		    }
 	    }
@@ -34,52 +34,31 @@ node {
 		    }
 	    }
     }
-    stage("Tag Image"){ 
 
-timestamps { 
-
-ws { 
-
-sh ''' 
-
-docker tag artemis:${Version} 965334959964.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
-''' 
-
-}
- }
-	    }
-  stage("Push Image"){ 
-
-timestamps { 
-
-ws { 
-
-sh ''' 
-
-docker push 965334959964.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version} 
-
-''' 
-
-} 
-
-} 
-
-} 
-
-stage("Send slack notifications"){ 
-
-timestamps { 
-
-ws { 
-
-echo "Slack" 
-
-//slackSend color: '#BADA55', message: 'Hello, World!' 
-
-} 
-
-} 
-
-} 
-
-} 
+    stage("Tag Image"){
+		timestamps {
+			ws {
+				sh '''
+                    docker tag artemis:${Version} 965334959964.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
+                    '''
+				}
+			}
+		}
+    stage("Push Image"){
+	    timestamps {
+			ws {
+				sh '''
+					docker push 965334959964.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
+					'''
+				}
+			}
+		}
+	stage("Send slack notifications"){
+		timestamps {
+			ws {
+					echo "Slack"
+					//slackSend color: '#BADA55', message: 'Hello, World!'
+				}
+			}
+		}
+	}
